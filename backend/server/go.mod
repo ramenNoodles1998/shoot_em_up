da2ef2e45d444efe736e4e2b78119b2de0b49e2a
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2022-10-20 21:21:00 -0600

    added test web sockets.

---
+++ backend/server/go.mod
@@ -0,0 +1,5 @@
+module shoot_em_up/server
+
+go 1.19
+
+require github.com/gorilla/websocket v1.5.0
--- frontend/shoot-em-up/src/routes/CreateGamePage.svelte
+++ frontend/shoot-em-up/src/routes/CreateGamePage.svelte
@@ -2,9 +2,14 @@
     
 </script>
 
-<div class="home">
+<div class="main">
     
 </div>
 
 <style>
+    .main {
+        background-color: rgb(29, 58, 81);
+        height: 500px;
+        width: 100%;
+    }
 </style>
